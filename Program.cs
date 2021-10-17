e6b9d7790cd7847135805fef5a4d267c233ca39e
vishal.Alok <you@example.com>
2021-10-17 22:39:13 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Static_MEthod
+{
+    class Program
+    {
+        public static void withoutObj()
+        {
+            Console.WriteLine("Hello");
+        }
+        static void Main()
+        {
+            Program.withoutObj();
+            Console.ReadKey();
+        }
+    }
+}
